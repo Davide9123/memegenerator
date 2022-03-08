9545ac09f2d38bc9ffa2a27b0b16ba8ac2d41e64
Davide Lamberti <wrvxxdavidexx@hotmail.it>
2022-03-08 10:52:16 +0100

    perfezionato ux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Header from './components/header';
 import Meme from './components/meme';
+import Footer from './components/footer'
 
 
 export default function App() {
   return (
-    <div>
+    <div className="myCont">
       <Header />
       <Meme/>
+      <Footer/>
     </div>
   )
 };
---
+++ src/components/footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function footer(){
+    return (
+        <div className="footer">
+            <h4>© 2022, Dav9123</h4>
+        </div>
+    )
+}
